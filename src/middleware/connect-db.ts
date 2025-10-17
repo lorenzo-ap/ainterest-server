b968fda8ed1e1e8353d6528862c0010189a16ebe
lorenzo-ap <ap.lorenzo03@gmail.com>
2025-10-17 14:18:28 +0300

    feat: update vercel.json

--- src/middleware/connect-db.ts
+++ src/middleware/connect-db.ts
@@ -3,8 +3,5 @@ import mongoose from 'mongoose';
 export const connectDB = async (url: string) => {
 	mongoose.set('strictQuery', true);
 
-	mongoose
-		.connect(url)
-		.then(() => console.log('MongoDB connected'))
-		.catch((err) => console.log(err));
+	return mongoose.connect(url);
 };
